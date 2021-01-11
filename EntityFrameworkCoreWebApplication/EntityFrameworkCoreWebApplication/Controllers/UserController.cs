0482abc196dc5e74e59c977e91770ebd2042fdc9
Kiyotaka.Abe <kiyo7447@gmail.com>
2021-01-11 21:34:08 +0900

    プロジェクト ファイルを追加します。

---
+++ EntityFrameworkCoreWebApplication/EntityFrameworkCoreWebApplication/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using EntityFrameworkCoreWebApplication.DbContexts;
+using EntityFrameworkCoreWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCoreWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+	{
+        private MyDbContext myDbContext;
+
+        public UserController(MyDbContext context)
+        {
+            myDbContext = context;
+        }
+
+        [HttpGet]
+        public IList<User> Get()
+        {
+            return (this.myDbContext.Users.ToList());
+        }
+    }
+}
